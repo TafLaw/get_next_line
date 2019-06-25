8ec944a860953628463a1032f61af70d3b983cc5
Tafadzwa MUZERENGANI <tmuzeren@c5r2s10.wethinkcode.co.za>
2019-06-25 18:18:27 +0200

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 11:27:39 by tmuzeren          #+#    #+#             */
-/*   Updated: 2019/06/25 12:39:30 by tmuzeren         ###   ########.fr       */
+/*   Updated: 2019/06/25 17:13:21 by tmuzeren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int		step(char **line, char *str, unsigned int size)
 	char			*temp;
 
 	i = 0;
-	printf("\n%d\n", size);
 	temp = ft_strdup(str);
 	while (str[i] != '\0' && i < size)
 	{
@@ -27,7 +26,10 @@ int		step(char **line, char *str, unsigned int size)
 		i++;
 	}
 	*line = ft_strsub(temp, 0, i);
-	free(temp);
+	ft_strdel(&temp);
+	if (ft_strlen(*line) == 0)
+		return (0);
+	free(*line);
 	return (1);
 }
 
@@ -36,21 +38,20 @@ int		get_next_line(const int fd, char **line)
 	//int				count;
 	char			buff[BUFF_SIZE + 1];
 	static char		*str;
-	char			*temp;
-
+	
 	str = "";
-	if (fd < 0)
+	if (fd < 0 || line == NULL || read(fd, buff, 0) < 0)
 		return (-1);
-	if (fd == 0)
-		return (0);
+	/*if (fd == 0)
+		return (0);*/
 	while (read(fd, buff, BUFF_SIZE) > 0)
 	{
 		buff[BUFF_SIZE] = '\0';
 		str = ft_strjoin(str, buff);
-		temp = str;
 		free(str);
 		if (ft_strchr(buff, '\n'))
 			break ;
+		ft_strclr(buff);
 	}
 	return (step(line, str, ft_strlen(str)));
 }
